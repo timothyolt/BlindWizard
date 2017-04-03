cb9872c3093b8378fb8b79247eed9f35eb5994b8
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-04-03 00:55:56 -0400

    Cast/Chase animation

--- Assets/Scripts/WizWave.cs
+++ Assets/Scripts/WizWave.cs
@@ -3,41 +3,48 @@ using UnityEngine;
 
 public class WizWave : MonoBehaviour
 {
-    [SerializeField]
-    private float time;
+    private float _time;
+    private Vector3 _startPosition;
+
+    private void Start ()
+	{
+	    _time = 0;
+	    _startPosition = transform.localPosition;
+	}
 
     [SerializeField]
     private Light _spotlight;
     [SerializeField]
-    private float angle;
+    private float _angle;
     [SerializeField]
-    private float distance;
+    private float _distance;
     [SerializeField]
-    private float travelRate;
+    private float _castRate;
+    [SerializeField]
+    private float _chaseRate;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    time = 0;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    private void Update ()
 	{
 	    if (_spotlight == null) return;
-	    if (time < distance)
+	    if (_time < _distance)
 	    {
-	        _spotlight.spotAngle = angle;
-	        _spotlight.range = time;
+	        _spotlight.spotAngle = _angle;
+	        _spotlight.range = _time;
+	        transform.localPosition = _startPosition;
+	        _time += Time.deltaTime * _castRate;
 	    }
-	    else if (time < distance * 2)
+	    else if (_time < _distance * 2)
 	    {
-	        var opposite = Math.Tan(Math.PI * (angle / 2d) / 180d) * distance;
-	        var rad = Math.Atan(opposite / (distance - time) * -1d);
-	        _spotlight.spotAngle = (float) (rad * (180d / Math.PI));
-	        _spotlight.range = distance;
+	        var opposite = Math.Tan(Math.PI * (_angle / 2d) / 180d) * _distance;
+	        var inverseDistance = _distance * 2d - _time;
+	        var rad = Math.Atan(opposite / inverseDistance);
+	        _spotlight.spotAngle = (float) (rad * (180d / Math.PI) * 2d);
+	        _spotlight.range = (float) inverseDistance;
+	        var offset = _startPosition;
+	        offset.z += (float) (_distance - inverseDistance);
+	        transform.localPosition = offset;
+	        _time += Time.deltaTime * _chaseRate;
 	    }
-	    else time -= distance * 2;
-	    time += Time.deltaTime * travelRate;
+	    else _time = 0;
 	}
 }
