0e5a139cb47a2a45b3edeb99e0f4dd6c7f2a2f21
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-04-18 18:31:30 -0400

    Wrap scores screen in every direction

--- Assets/Scripts/ManageScores.cs
+++ Assets/Scripts/ManageScores.cs
@@ -4,17 +4,23 @@ using UnityEngine.UI;
 
 public class ManageScores : MonoBehaviour {
     [SerializeField]
-    private Text _kills, _turns, _shimmers, _blocks, _floors;
+    private Text[] _kills, _turns, _shimmers, _blocks, _floors;
 
     private void Start () {
     }
 
-    private void Update () {
-        _kills.text = "Kills: " + Score.Kills;
-        _turns.text = "Turns: " + Score.Turns;
-        _blocks.text = "Blocks: " + Score.Blocks;
-        _floors.text = "floors: " + Score.Floors;
-        _shimmers.text = "shimmers: " + Score.Shimmers;
+    private void Update ()
+    {
+        foreach (var kills in _kills)
+            kills.text = "Kills: " + Score.Kills;
+        foreach (var turns in _turns)
+            turns.text = "Turns: " + Score.Turns;
+        foreach (var blocks in _blocks)
+            blocks.text = "Blocks: " + Score.Blocks;
+        foreach (var floors in _floors)
+            floors.text = "floors: " + Score.Floors;
+        foreach (var shimmers in _shimmers)
+            shimmers.text = "shimmers: " + Score.Shimmers;
 
         if (GvrController.AppButtonDown)
             SceneManager.LoadScene(0);
