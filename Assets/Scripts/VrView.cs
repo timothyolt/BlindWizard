6195cc61abb188d1bca94bc3620be154cb15ef25
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-04-27 01:01:51 -0400

    Got NoVr and Daydream switching working
    Open app from the daydream app/home screen to use vr. Open app from elsewhere to use NoVr mode.

--- Assets/Scripts/VrView.cs
+++ Assets/Scripts/VrView.cs
@@ -1,21 +1,65 @@
-﻿using System.Security.AccessControl;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.VR;
 
-public class VrView : MonoBehaviour {
+public class VrView : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _googleVrObjects;
 
-	// Use this for initialization
+    [SerializeField]
+    private bool _stereo;
+    public bool Stereo
+    {
+        get => _stereo;
+        set
+        {
+            _stereo = value;
+            if (_stereo) {
+                VRSettings.LoadDeviceByName("daydream");
+                VRSettings.enabled = true;
+                // Reset NoVr offset
+                transform.parent.localRotation = Quaternion.Euler(0, 0, 0);
+                transform.localPosition = new Vector3(0, 2, 0);
+                // Enable any special GVR stuff
+                if (_googleVrObjects == null) return;
+                foreach (var googleVrObject in _googleVrObjects)
+                    googleVrObject?.SetActive(true);
+            }
+            else
+            {
+                VRSettings.LoadDeviceByName("None");
+                VRSettings.enabled = false;
+                Input.gyro.enabled = true;
+                Screen.autorotateToPortraitUpsideDown = false;
+                Screen.autorotateToPortrait = false;
+                // Set NoVr offset
+                // TODO (timothyolt): settings adjustable offset. Idk for dorks who play laying down.
+                transform.parent.localRotation = Quaternion.Euler(90, 0, 0);
+                transform.localPosition = new Vector3(0, 0, -1);
+                // Disable any special GVR stuff
+                if (_googleVrObjects == null) return;
+                foreach (var googleVrObject in _googleVrObjects)
+                    googleVrObject?.SetActive(false);
+            }
+        }
+    }
+
     private void Start ()
     {
-        Input.gyro.enabled = true;
-        Screen.autorotateToPortraitUpsideDown = false;
-        Screen.autorotateToPortrait = false;
+        // TODO (timothyolt): settings override
+        Debug.Log("yoyoyo " + GvrIntent.GetData());
+        Stereo = GvrIntent.IsLaunchedFromVr();
     }
-	
-	// Update is called once per frame
+
     private void Update ()
     {
-        transform.localRotation = new Quaternion(
+        if (!Stereo)
+        {
+            transform.localPosition = new Vector3(0, 0, -1);
+            transform.localRotation = new Quaternion(
                 Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
-        Debug.Log(Input.gyro.attitude.eulerAngles);
+        }
+        else
+            transform.localPosition = new Vector3(0, 1, 0);
     }
 }
