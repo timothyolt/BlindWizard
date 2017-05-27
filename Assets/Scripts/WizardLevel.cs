9f8d9877b658cdd335496e4b8d0f10486af6357f
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-05-26 22:09:41 -0400

    Switch from material instancing to static merging

--- Assets/Scripts/WizardLevel.cs
+++ Assets/Scripts/WizardLevel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
